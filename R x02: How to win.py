73098596f6a8292216d5701d6b0c50fcf9dfc5bd
UJJAWAL KUMAR <76205574+ujjawal-kumar17@users.noreply.github.com>
2023-03-17 14:01:55 +0530

    Update R x02: How to win.py

--- R x02: How to win.py
+++ R x02: How to win.py
@@ -1,19 +1,10 @@
 n,hit,t=map(int,input().split())
-2
 l = sorted([int(i) for i in input().split()[0:n]])
-3
 res,h=0,0
-4
 for i in l:
-5
     h = i // hit+(1 if i%hit else 0)
-6
     if t >= h:
-7
         t -= h
-8
         res += 1
-9
     else: break
-10
 print(res)
