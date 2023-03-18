498db332a8c111409146f9642fb773c40f30fb0b
UJJAWAL KUMAR <76205574+ujjawal-kumar17@users.noreply.github.com>
2023-03-18 11:08:30 +0530

    Add files via upload

--- CCT A5 - Kefa hates coins.c
+++ CCT A5 - Kefa hates coins.c
@@ -2,40 +2,37 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-
-int compare(const void *x,const void *y)
-{
-    return *(int*)x - *(int*)y;
+int cmp(const void *a,const void *b){
+    return(*(int*)a-*(int*)b);
 }
-
-int main() 
-{
-    int n, gc;
-    scanf("%d %d",&n,&gc);
-    
-    int coin[n], i;
-    for(i=0;i<n;i++)
-        scanf("%d",&coin[i]);
-    
-    qsort(coin,n,sizeof(int),compare);
-    
-    int j = 0, flag = 0;
-    
-    for(i=0;i<gc;i++)
-    {
-        int giftValue, sum = 0, coinCount = 0;
-        scanf("%d",&giftValue);
-        while(sum < giftValue)
-        {
-            if(j == n)
-            {
-                flag = 1;
+int main() {
+int m,n;
+    scanf("%d%d",&n,&m);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    qsort(a,n,sizeof(int),cmp);
+    int v;
+    for(int i=0,j=0;i<m;i++){
+        scanf("%d",&v);
+        int s=0,f=0,c=0;
+        while(s<v){
+            if(j==n){
+                f=1;
                 break;
             }
-            sum += coin[j++];
-            coinCount++;
+            s=s+a[j];
+            j++;
+            c++;
+            
         }
-        flag ? printf("-1 -1\n") : printf("%d %d\n",coinCount,sum);
+        if(f==1){
+            printf("%d %d\n",-1,-1);
+        }
+        else
+            printf("%d %d\n",c,s);
     }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
